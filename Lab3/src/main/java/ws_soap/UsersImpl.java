5de2031c2ab8b3e9b2a0e099a5f128330ff840c4
Svedas <Svedas@users.noreply.github.com>
2019-05-22 13:51:25 +0300

    Add files via upload

--- Lab3/src/main/java/ws_soap/UsersImpl.java
+++ Lab3/src/main/java/ws_soap/UsersImpl.java
@@ -9,8 +9,11 @@ import javax.jws.WebService;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -70,9 +73,34 @@ public class UsersImpl implements Users{
     } 
 
     @Override
-    public returnDetailedMessage addUser(String email, String role, String accessLevel) {
+    public returnDetailedMessage addUser(String email, String role, String accessLevel) { // padaryt kad jei userio nera, pridetu ji i frendservice, ir sujungti su putUser
         returnDetailedMessage returnval = null;
         try {
+            
+            // Check user in friend service
+    //        System.out.println(email);
+            boolean userFound = true;
+            URL url1 = new URL("http://friendservice:5000/users/" + email);
+            HttpURLConnection conn1 = (HttpURLConnection) url1.openConnection();
+            conn1.setRequestMethod("GET");
+            conn1.setRequestProperty("Accept", "application/json");
+            if (conn1.getResponseCode() > 300) {
+                userFound = false;
+            }
+            conn1.disconnect();
+            
+            // Check user in web service
+            boolean userExist = true;
+            URL url2 = new URL("http://friendservice:5000/users/" + email);
+            HttpURLConnection conn2 = (HttpURLConnection) url2.openConnection();
+            conn2.setRequestMethod("GET");
+            conn2.setRequestProperty("Accept", "application/json");
+            if (conn1.getResponseCode() != 200) {
+                userExist = false;
+            }
+            conn2.disconnect();
+            
+            
             String jsonPutString = "{";
             if (email != null)
                 jsonPutString+= "\"email\": \"" + email + "\",";
@@ -82,12 +110,60 @@ public class UsersImpl implements Users{
             
             if (accessLevel != null)
                 jsonPutString += "\"accessLevel\": \"" + accessLevel + "\",";
+            
+            if(!userFound){
+                // Make a user
+                jsonPutString+= "\"firstName\": \"" + "firstName" + "\",";
+                jsonPutString += "\"lastName\": \"" + "lastName" + "\",";
+            }
 
             if(jsonPutString.endsWith(","))
             {
                 jsonPutString = jsonPutString.substring(0,jsonPutString.length() - 1);
             }
             jsonPutString += "}";
+            System.out.println(jsonPutString);
+            
+            // Updates user
+            if(userExist){
+                return putUser(email, role, accessLevel);
+            }
+            
+            
+            /*if(!userFound){
+                // Make a user
+                jsonPutString+= "\"firstName\": \"" + "firstName" + "\",";
+                jsonPutString += "\"lastName\": \"" + "lastName" + "\",";
+                if(jsonNewUser.endsWith(","))
+                {
+                    jsonPutString = jsonPutString.substring(0,jsonPutString.length() - 1);
+                }
+                jsonPutString += "}";
+                String jsonNewUser = "{";
+                jsonNewUser+= "\"firstName\": \"" + "naujas_vardas" + "\",";
+                jsonNewUser+= "\"lastName\": \"" + "nauja_pavarde" + "\",";
+                jsonNewUser+= "\"email\": \"" + email + "\",";
+                if(jsonNewUser.endsWith(","))
+                {
+                    jsonNewUser = jsonNewUser.substring(0,jsonNewUser.length() - 1);
+                }
+                jsonNewUser += "}";
+                
+                URL url = new URL("http://friendservice:5000/users");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setDoOutput(true);
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Accept", "application/json");
+                conn.setRequestProperty("Content-Type", "application/json");
+                OutputStreamWriter out = new OutputStreamWriter(conn.getOutputStream());
+                out.write(jsonNewUser);
+                out.close();
+                if (conn.getResponseCode() > 300) {
+                throw new RuntimeException("Failed : HTTP error code from REST service was returned: "
+                        + conn.getResponseCode());
+                }
+                conn.disconnect();*/
+            
             URL url = new URL("http://web:5001/users");
 //            URL url = new URL("http://0.0.0.0:5001/users");
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -98,17 +174,18 @@ public class UsersImpl implements Users{
             OutputStreamWriter out = new OutputStreamWriter(conn.getOutputStream());
             out.write(jsonPutString);
             out.close();
-//            conn.getInputStream(); // questionable
-//            System.out.println(jsonPutString);
+            conn.getInputStream(); // questionable
+            System.out.println(jsonPutString);
 //            conn.getInputStream();
+            String jsonString = JSONConverter.getJsonStringFromConnection(conn);
+
+            returnval = JSONConverter.jsonToReturnDetailedMsg(jsonString);
             if (conn.getResponseCode() > 300) {
                 throw new RuntimeException("Failed : HTTP error code from REST service was returned: "
                         + conn.getResponseCode());
             }
-            String jsonString = JSONConverter.getJsonStringFromConnection(conn);
-            returnval = JSONConverter.jsonToReturnDetailedMsg(jsonString);
-            
             conn.disconnect();
+            
         } catch (ProtocolException e) {
             e.printStackTrace();
             returnval = new returnDetailedMessage();
@@ -184,15 +261,13 @@ public class UsersImpl implements Users{
                     conn.getOutputStream());
             out.write(jsonPutString);
             out.close();
-//            conn.getInputStream(); // questionable
-            
+            conn.getInputStream();
+            String jsonString = JSONConverter.getJsonStringFromConnection(conn);
+            returnval = JSONConverter.jsonToReturnDetailedMsg(jsonString);
             if (conn.getResponseCode() > 300) {
                 throw new RuntimeException("Failed : HTTP error code from REST service was returned: "
                         + conn.getResponseCode());
             }
-            String jsonString = JSONConverter.getJsonStringFromConnection(conn);
-            returnval = JSONConverter.jsonToReturnDetailedMsg(jsonString);
-            
             conn.disconnect();
         } catch (ProtocolException e) {
             e.printStackTrace();
@@ -223,7 +298,8 @@ public class UsersImpl implements Users{
             conn.setRequestMethod("DELETE");
             conn.setRequestProperty("Accept", "application/x-www-form-urlencoded");
             conn.connect();
-            
+            String jsonString = JSONConverter.getJsonStringFromConnection(conn);
+            //returnval = JSONConverter.jsonToReturnMsg(jsonString);
             if (conn.getResponseCode() == 204) {
                 return new returnMessage("Successfully deleted user with email: " + email);
             }
@@ -231,9 +307,6 @@ public class UsersImpl implements Users{
                 throw new RuntimeException("Failed : HTTP error code from REST service was returned: "
                         + conn.getResponseCode());
             }
-            String jsonString = JSONConverter.getJsonStringFromConnection(conn);
-            //returnval = JSONConverter.jsonToReturnMsg(jsonString);
-            
             conn.disconnect();
         } catch (ProtocolException e) {
             throw new RuntimeException("bad input");
